8f1719522bd15ab60e72146424827c72b210647e
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-10 15:10:26 +0600

    golang's while is for

--- 12-loops/main.go
+++ 12-loops/main.go
@@ -6,9 +6,17 @@ func main() {
 
 	// basic for loop
 
-	for i := 0; i < 10; i++ {
-		fmt.Println(i);
-	}
+	/*
+		for i := 0; i < 10; i++ {
+			fmt.Println(i);
+		}
+	*/
 
+	var num int = 10;
+
+	for num >= 0 {
+		fmt.Println(num);
+		num--;
+	}
 	
 }
