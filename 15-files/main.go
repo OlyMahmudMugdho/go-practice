2e370b11e72bc6f1501e5235c28d8baf35c6b448
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-12 12:42:39 +0600

    reading file

--- 15-files/main.go
+++ 15-files/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	var content string = "sample text 2";
+	var content string = "sample text";
 	file,err := os.Create("./some.txt");
 
 	if err != nil {
@@ -21,6 +21,19 @@ func main() {
 
 	fmt.Println("the length of the content is ", length);
 
+
+	readFile()
+
 	defer file.Close();
 	
+}
+
+
+func readFile() {
+	dataBytes,_ := os.ReadFile("./some.txt");
+	fmt.Println("The data bytes in the file : ");
+	fmt.Println(dataBytes);
+
+	fmt.Println("The text in the file : ");
+	fmt.Println(string(dataBytes));
 }
