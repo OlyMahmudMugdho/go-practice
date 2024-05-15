21a6771da74845af48fda793d6244b5254dc1cd4
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-15 18:45:50 +0600

    unformatted marshalling

--- 17-json-forming/main.go
+++ 17-json-forming/main.go
@@ -12,10 +12,22 @@ type Todo struct {
 	Completed bool   `json:"completed"`
 }
 
+type User struct {
+	Id       int
+	Name     string
+	Roll     int
+	password string
+	// password will not included in json
+}
+
 func main() {
 
 	taskOne := Todo{1, 1, "something", false}
 	todoJson, _ := json.MarshalIndent(taskOne, "", "\t")
 	fmt.Println(string(todoJson))
+
+	user1 := User{1, "Mugdho", 14, "abc"}
+	userJson, _ := json.Marshal(user1)
+	fmt.Println(string(userJson))
 
 }
