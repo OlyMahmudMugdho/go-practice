291b435e42f6be173efe785ac4ac65ddfcd9f1b6
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-17 12:15:12 +0600

    node.js server for golang json practice

---
+++ 19-node-server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+
+app.get("/get-end", (req,res) => {
+    res.json({
+        message : "hello world"
+    })
+})
+
+app.post("/post-end", (req,res) => {
+    const data = req.body;
+    res.json(data)
+} )
+
+
+app.listen(3000, () => {
+    console.info("server is running on port 3000")
+})
