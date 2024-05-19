1a3db3abd0fdf8896097782cee4e6c9fc3e7053c
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-19 22:41:43 +0600

    simple api

---
+++ 21-routing/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Println("working...")
+	r := mux.NewRouter()
+	r.HandleFunc("/", sayHello).Methods("GET")
+	log.Fatal(http.ListenAndServe(":5000", r))
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request)  {
+	json.NewEncoder(w).Encode("running server")
+}
