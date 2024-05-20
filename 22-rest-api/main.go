e1d96ea535105a4f4d52ff42c4e814f0d6d537b6
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-21 00:18:56 +0600

    get single book

--- 22-rest-api/main.go
+++ 22-rest-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,19 +10,46 @@ import (
 )
 
 type Book struct {
-	Id     string `json:book_id`
+	Id     string    `json:book_id`
 	Name   string `json:name`
 	Author string `json:author`
 }
 
+
+
+
+
+var books []Book
+
 func main() {
-	router := mux.NewRouter();
+	router := mux.NewRouter()
 	router.HandleFunc("/", serveHomePage).Methods("GET")
+	router.HandleFunc("/books/{id}", getOneBook).Methods("GET")
+
+	books = append(books, Book{"1", "Spring in Action", "Craig Walls"})
+
 	fmt.Println("server is running on port 3000")
 	log.Fatal(http.ListenAndServe(":3000", router))
-	
+
 }
 
 func serveHomePage(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("This is the home page"))
+}
+
+func getOneBook(w http.ResponseWriter, r *http.Request) {
+
+	params := mux.Vars(r)
+	
+
+	for _, book := range books {
+		if params["id"] == book.Id {
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	
+	w.WriteHeader(404)
+
+	return
 }
