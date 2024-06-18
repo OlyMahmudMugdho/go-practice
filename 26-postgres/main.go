9ab028cd0586718a7d2278dde209c75b274f7fe6
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-18 17:27:21 +0600

    create table

--- 26-postgres/main.go
+++ 26-postgres/main.go
@@ -10,9 +10,25 @@ import (
 
 func main() {
 	fmt.Println("hello")
-	connStr := "user=postgres dbname=postgres password=mysecretpassword"
-	_, err := sql.Open("postgres", connStr)
+	connStr := "user=postgres dbname=postgres password=mysecretpassword sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	table, err := db.Query(`
+		create table books (
+			id int,
+			name varchar(255),
+			author varchar(255)
+		)
+	`)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(table)
+	defer db.Close()
+
 }
