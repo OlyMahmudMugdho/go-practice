00538f65097eb0eef626c3450816fb86e2c599dc
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-18 17:41:09 +0600

    organized into functions

--- 26-postgres/main.go
+++ 26-postgres/main.go
@@ -11,30 +11,25 @@ import (
 func main() {
 	fmt.Println("hello")
 	connStr := "user=postgres dbname=postgres password=mysecretpassword sslmode=disable"
+
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	/* table, err := db.Query(`
-		create table books (
-			id int,
-			name varchar(255),
-			author varchar(255)
-		)
-	`)
+	rows, err := createTable(db)
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(table) */
+	fmt.Println(rows)
 
-	rows, err := droptTable(db)
-	if err != nil {
-		fmt.Println(err)
-	}
-	print(rows)
+	// rows, err := droptTable(db)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// print(rows)
 
 	defer db.Close()
 
@@ -43,6 +38,17 @@ func main() {
 func droptTable(db *sql.DB) (*sql.Rows, error) {
 	rows, err := db.Query(`
 	  	drop table books
+	`)
+	return rows, err
+}
+
+func createTable(db *sql.DB) (*sql.Rows, error) {
+	rows, err := db.Query(`
+		create table books (
+			id int,
+			name varchar(255),
+			author varchar(255)
+		)
 	`)
 	return rows, err
 }
