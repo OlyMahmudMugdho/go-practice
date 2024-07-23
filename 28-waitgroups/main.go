8fdb86a5f9344492082510ece8adcdc95e9c8a91
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-23 13:19:35 +0600

    factorial

--- 28-waitgroups/main.go
+++ 28-waitgroups/main.go
@@ -10,10 +10,19 @@ var wg sync.WaitGroup
 func main() {
 	go greet("good morning")
 	wg.Add(1)
+
 	wg.Wait()
 }
 
 func greet(message string) {
 	defer wg.Done()
 	fmt.Println(message)
+}
+
+func factorial(num int) int {
+	defer wg.Done()
+	if num == 0 || num == 1 {
+		return 1
+	}
+	return num * factorial(num-1)
 }
