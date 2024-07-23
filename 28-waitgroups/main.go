01946562e6a03e0951d9b209fb98afa23fa08a16
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-23 13:56:14 +0600

    a good example

--- 28-waitgroups/main.go
+++ 28-waitgroups/main.go
@@ -13,11 +13,38 @@ func main() {
 	go greet("good morning")
 	wg.Add(1)
 
+	// example use case
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, v := range nums {
+		go DoFactorial(v)
+		wg.Add(1)
+	}
+
+	// wait for the operations
 	wg.Wait()
+
+	// print the results after completion
+	for _, v := range factorialResults {
+		fmt.Printf("%v \n", v)
+	}
 
 }
 
 func greet(message string) {
 	defer wg.Done()
 	fmt.Println(message)
+}
+
+func factorial(num int) int {
+	if num == 0 || num == 1 {
+		return 1
+	}
+	return num * factorial(num-1)
+}
+
+func DoFactorial(num int) {
+	defer wg.Done()
+	var res int = factorial(num)
+	factorialResults = append(factorialResults, map[int]int{num: res})
 }
