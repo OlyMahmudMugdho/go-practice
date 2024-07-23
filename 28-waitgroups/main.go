39dfc84f3f8520141e143144c6dd8d9676db2b57
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-23 13:54:36 +0600

    simple example of wait groups

--- 28-waitgroups/main.go
+++ 28-waitgroups/main.go
@@ -6,23 +6,18 @@ import (
 )
 
 var wg sync.WaitGroup
+var factorialResults = []map[int]int{}
 
 func main() {
+	// simple example
 	go greet("good morning")
 	wg.Add(1)
 
 	wg.Wait()
+
 }
 
 func greet(message string) {
 	defer wg.Done()
 	fmt.Println(message)
-}
-
-func factorial(num int) int {
-	defer wg.Done()
-	if num == 0 || num == 1 {
-		return 1
-	}
-	return num * factorial(num-1)
 }
